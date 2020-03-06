0cc652a2bfb8cbecdba6b513196d6891c7fd906a
E <e@es-macbook-pro.lan>
2020-03-06 09:13:49 -0700

    new stuff

---
+++ maths function loop.js
@@ -0,0 +1,14 @@
+const makeNoise = function(){
+    console.log("pling!");
+}
+makeNoise();
+
+const power = function(base,exponent){
+    let result =1;
+
+for(let count =0; count<exponent;count++){
+    result *= base
+}
+return result;
+}
+console.log(power(2,10))
