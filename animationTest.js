66276f5775dce096e3e9c13f5a6ca2815bda2138
e <e@lovelys-mbp.lan>
2020-03-17 20:24:33 -0700

    added hp attack message in img div

--- animationTest.js
+++ animationTest.js
@@ -10,5 +10,33 @@ $(".myButtonL").click(function(){
 $(".myButtonA").click(function(){
     $("#jiggly").animate({left: "0"}, "fast");
     $("#jiggly").animate({left: "800px"}, "slow");
-
+    // $(".jigglyA").animate({
+    //     left: '250px',
+    //     opacity: '0.5',
+    //     height: '150px',
+    //     width: '150px'})
 });
+
+$(".myButtonA").click(function(){
+    $("#jigglyA").fadeIn(1000);
+    // $("#jigglyA").delay(2000)
+    $("#jigglyA").fadeOut(2000)
+});
+
+
+// function donus() {
+
+//     var quotes = $(".jigglyA");
+//     var quoteIndex = -1;
+
+//     function showNextQuote() {
+//         ++quoteIndex;
+//         quotes.eq(quoteIndex % quotes.length)
+//             .fadeIn(2000)
+//             .delay(2000)
+//             .fadeOut(2000, showNextQuote);
+//     }
+
+//     showNextQuote();
+
+// }
